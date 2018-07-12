bbf7b51a5879071fbac2d7b6e987a30173cce8e8
Dinuka Jayasuriya <dinukapps@gmail.com>
2018-07-13 01:38:10 +0800

    Initial commit

---
+++ public/js/slack.js
@@ -0,0 +1,23 @@
+
+function join_slack() {
+    var email = $('#page_controls_email').val();
+    if (email.length > 0) {
+        $.ajax({
+            dataType: 'json',
+            processData: false,
+            type: 'POST',
+            url: 'https://slack.com/api/users.admin.invite?token=xoxp-394441928593-397352076484-398634338967-9695fc8930abc0f738f3f2599dc231e0&email=' + email,
+            success: function (msg) {
+                notie.alert(1, 'Invitation sent! Check your Email 😃', 5);
+                console.log(msg);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                notie.alert(3, errorThrown, 3);
+                console.log(errorThrown);
+            }
+        });
+    }
+    else {
+        notie.alert(2, 'Enter your email address 👇', 3);
+    }
+}
